bc94c69ebdb28cd07329ed3b29aa38cfe0e8be78
Rui Teixeira <rui@vazteixeira.org>
2023-12-03 17:28:48 +0000

    add .env.sample

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -10,7 +10,10 @@ const config: Options = {
   entitiesTs: ["./entities"],
   metadataProvider: TsMorphMetadataProvider,
   type: "postgresql",
-  clientUrl: process.env.PSQL_CONN,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number.parseInt(process.env.DB_PORT!),
   dbName: process.env.DB_NAME,
   cache: {
     enabled: true,
